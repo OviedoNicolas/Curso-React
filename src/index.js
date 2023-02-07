daf6514c96d83a4e8615d4d2e3fa899c23afab09
Nicolas Oviedo <nicoohdeoviedo@gmail.com>
2023-02-07 11:37:15 -0300

    se borraron archivos innecesarios

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <h1>Holanda</h1>
-    <h2>K ase</h2>
   </React.StrictMode>
 );
 
